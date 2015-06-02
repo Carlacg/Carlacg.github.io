b0def71fbedc1962437a3414a076717caef5c6dc
Carlacg <carlacg.cadf@gmail.com>
2015-06-01 20:27:07 -0500

    foodcourt

---
+++ javascript/banner.js
@@ -0,0 +1,11 @@
+document.writeln( '<div id="banner">' );
+document.writeln( '<a href="index.html"><img id="imagen" src="imagenes/banner.png" alt="FoodCourt" /></a>' );
+document.writeln( '<nav id="menu">' );
+document.writeln( '<ul><li class="inicio"><a href="index.html">Inicio</a></li>' );
+document.writeln( '<li><a class="receta" href="recetas.html">Recetas</a>' );
+document.writeln( '<ul><li><a href="recetas.html#entradas">Entradas</a></li><li><a href="recetas.html#platosF">Platos Fuertes</a></li><li><a href="recetas.html#postres">Postres</a></li><li><a href="recetas.html#bebidas">Bebidas</a></ul></li>');
+document.writeln( '<li><a class="acerca"href="acerca.html">Acerca De</a></li>' );
+document.writeln( '<li><a class="contacto" href="contacto.html">Contacto</a></li></ul>' );
+document.writeln( '</nav></div>' );
+
+
---
+++ javascript/post_recientes.js
@@ -0,0 +1,9 @@
+document.writeln( '<div id="contSecundario">');
+document.writeln( '<div id="buscar"> <form method="get">');
+document.writeln( '<label for="buscador">Buscar :</label> <input id="buscador" type="text"/><a href="#"><img class="buscador" src="imagenes/iconos/magnify.png" alt="buscar"/></a>');
+document.writeln( '</form></div>');
+document.writeln( '<h4>Post recientes</h4>');
+document.writeln( '<p><a class="img" href="#"><img src="imagenes/preview/1.png" alt="quesadilla"/></a> <a class="link" href="#">Quesadillas de Camarón con Queso Parmesano</a>');
+document.writeln( '<a class="img" href="#"><img src="imagenes/preview/2.png" alt="smoothie"/></a> <a class="link" href="#">Smoothie Tropical</a>');
+document.writeln( '<a class="img" href="#"><img src="imagenes/preview/3.png" alt="cupcake"/></a> <a class="link" href="#">Cupcakes de limón y merengue</a>');
+document.writeln( '</p> </div>');
