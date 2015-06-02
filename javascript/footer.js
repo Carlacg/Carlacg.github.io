adddc8a3f6b6ff98fe984226ec20823425d5f5f8
Carlacg <carlacg.cadf@gmail.com>
2015-06-02 10:29:27 -0500

    cambio pequeño de estilo

--- javascript/footer.js
+++ javascript/footer.js
@@ -1,4 +1,3 @@
 document.writeln( '<footer><a href=""><img src="imagenes/iconos/facebook-box.png" alt="Facebook"/></a>');
 document.writeln( '<a href=""><img src="imagenes/iconos/twitter-box.png" alt="Twitter"/></a>');
-document.writeln( '<a href=""><img src="imagenes/iconos/google-plus-box.png" alt="Google+"/></a>');
 document.writeln( '<p>&copy; FoodCourt</p></footer>');
