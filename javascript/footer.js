7ba54a1294059268c471f42c951484159a3f0dc4
Carlacg <carlacg.cadf@gmail.com>
2015-06-04 19:07:27 -0500

    responsive

--- javascript/footer.js
+++ javascript/footer.js
@@ -1,3 +1,4 @@
-document.writeln( '<footer><a href=""><img src="imagenes/iconos/facebook-box.png" alt="Facebook"/></a>');
-document.writeln( '<a href=""><img src="imagenes/iconos/twitter-box.png" alt="Twitter"/></a>');
+document.writeln( '<footer><a href="https://www.facebook.com/davcocom?fref=ts"><img src="imagenes/iconos/facebook-box.png" alt="Facebook"/></a>');
+document.writeln( '<a href="https://twitter.com/DavCocom"><img src="imagenes/iconos/twitter-box.png" alt="Twitter"/></a>');
+document.writeln( '<a href="https://plus.google.com/+DavidCocom/posts"><img src="imagenes/iconos/google-plus-box.png" alt="Google+"/></a>');
 document.writeln( '<p>&copy; FoodCourt</p></footer>');
