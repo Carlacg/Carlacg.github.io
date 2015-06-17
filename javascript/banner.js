a2fcc75f842861da3ab27ecf61ead00b57efec55
Carla <carlacg.cadf@gmail.com>
2015-06-17 10:52:27 -0500

    scroll to top

--- javascript/banner.js
+++ javascript/banner.js
@@ -2,7 +2,7 @@ document.writeln( '<div id="banner">' );
 document.writeln( '<header><a href="index.html"><img id="imagen" src="imagenes/banner.png" alt="FoodCourt" /></a></header>' );
 document.writeln( '<nav id="menu">' );
 document.writeln( '<ul><li class="inicio"><a href="index.html">Inicio</a></li>' );
-document.writeln( '<li><a class="receta" href="recetas.html">Recetas</a>' );
+document.writeln( '<li class="receta"><a class="receta" href="recetas.html">Recetas</a>' );
 document.writeln( '<ul><li><a href="recetas.html#entradas">Entradas</a></li><li><a href="recetas.html#platosF">Platos Fuertes</a></li><li><a href="recetas.html#postres">Postres</a></li><li><a href="recetas.html#bebidas">Bebidas</a></ul></li>');
 document.writeln( '<li><a class="acerca"href="acerca.html">Acerca De</a></li>' );
 document.writeln( '<li><a class="contacto" href="contacto.html">Contacto</a></li></ul>' );
