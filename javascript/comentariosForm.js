711e43bac27e0250b8282edb5f273fb358eb8ccc
Carla <carlacg.cadf@gmail.com>
2015-06-23 16:57:23 -0500

    d

--- javascript/comentariosForm.js
+++ javascript/comentariosForm.js
@@ -4,7 +4,7 @@ var receta = elements[elements.length - 1].split("#")[0];
 
 document.writeln( '<div id="formulario">');
 document.writeln( '<h4> Deja tu comentario </h4>');
-document.writeln( '<form id="comment" action="" method="post">');
+document.writeln( '<form id="comment" action="">');
 document.writeln( '<div> <label for="nombre"> * Nombre: </label><input class="textbox" type="text" name="nombre" id="nombre" required/></div>');
 document.writeln( '<div> <label for="correo"> * E-mail: </label><input class="textbox" type="email" name="correo" id="correo" required/></div>');
 document.writeln( '<div> <label for="web"> Sitio Web: </label><input class="textbox" type="text" name="web" id="web"/></div>');
